0b57469eafd78a5ce921b0bafa74ceec1814e4f5
Samuel Sepeši <samuel.sepesi@student.tuke.sk>
2018-07-19 03:29:30 +0200

    Refactor HeaderMenu component

--- client/src/components/HeaderMenu.jsx
+++ client/src/components/HeaderMenu.jsx
@@ -3,254 +3,94 @@ import logo from '../img/logo.jpg';
 import '../styles/components/HeaderMenu.css';
 import { emptyCartIcon, hamburgerIcon, arrowDownIcon } from '../icons';
 
-class HeaderMenu extends React.Component {
-  render() {
-    return (
-      <div className="HeaderMenu">
-        <div className="HeaderMenu-logoAndIcon">
-          <a href="/home">
-            <img src={logo} alt="Nejfuton logo" />
-          </a>
-          <label title="Toggle Drop-down" class="HeaderMenu-hamburger" htmlFor="dropdownCheckbox">
-            {hamburgerIcon}
-          </label>
-        </div>
-        <input type="checkbox" id="dropdownCheckbox" />
-        <div className="HeaderMenu-mainContentResponsive">
-          <div className="HeaderMenu-mainCategories">
-            <div className="HeaderMenu-dropdown">
-              <a href="#">
-                Futony
-                <label title="Toggle Drop-down" class="drop-icon" for="sm1">
-                  {arrowDownIcon}
-                </label>
-              </a>
-              <input type="checkbox" id="sm1" />
-              <div className="sub-menu">
-                <a href="/futony/bavlna/">Bavlna</a>
-                <a href="/futony/kokos/">Kokos</a>
-                <a href="/futony/latex/">Latex</a>
-                <a href="/futony/detske/">Detské</a>
-                <a href="/futony/masazove/">K masážam</a>
-              </div>
-            </div>
-            <div className="HeaderMenu-dropdown">
-              <a href="#">
-                Pohovky
-                <label title="Toggle Drop-down" class="drop-icon" for="sm2">
-                  {arrowDownIcon}
-                </label>
-              </a>
-              <input type="checkbox" id="sm2" />
-              <div class="sub-menu">
-                <a href="/pohovky/80-90x200/">80-90x200 cm</a>
-                <a href="/pohovky/140x200/">140x200 cm</a>
-                <a href="/pohovky/160x200/">160x200 cm</a>
-              </div>
-            </div>
-            <div className="HeaderMenu-dropdown">
-              <a href="#">
-                Postele
-                <label title="Toggle Drop-down" class="drop-icon" for="sm3">
-                  {arrowDownIcon}
-                </label>
-              </a>
-              <input type="checkbox" id="sm3" />
-              <div className="sub-menu">
-                <a href="/postele/90x200/">90x200 cm</a>
-                <a href="/postele/140x200/">140x200 cm</a>
-                <a href="/postele/160x200/">160x200 cm</a>
-                <a href="/postele/180x200/">180x200 cm</a>
-              </div>
-            </div>
-            <div className="HeaderMenu-dropdown">
-              <a href="#">
-                Doplnky
-                <label title="Toggle Drop-down" class="drop-icon" for="sm4">
-                  {arrowDownIcon}
-                </label>
-              </a>
-              <input type="checkbox" id="sm4" />
-              <div class="sub-menu">
-                <a href="/doplnky/kresla/">Kreslá</a>
-                <a href="/doplnky/nabytok/">Nábytok</a>
-                <a href="/doplnky/vankuse/">Vankúše</a>
-              </div>
+const HeaderMenu = (props) => {
+  return (
+    <div className="HeaderMenu">
+      <div className="HeaderMenu-logoAndIconContainer">
+        <a href="/home">
+          <img src={logo} alt="Nejfuton logo" />
+        </a>
+        <label title="Show menu" className="HeaderMenu-hamburgerIcon" htmlFor="dropdownMainMenu">
+          {hamburgerIcon}
+        </label>
+      </div>
+      <input className="HeaderMenu-hiddenInput" type="checkbox" id="dropdownMainMenu" />
+      <div className="HeaderMenu-navigationLinks">
+        <div className="HeaderMenu-centerContainer">
+          <div className="HeaderMenu-dropdownLink">
+            <a href="#">
+              Futony{' '}
+              <label title="Show additional links" htmlFor="dropdownLinkOne">
+                {arrowDownIcon}
+              </label>
+            </a>
+            <input className="HeaderMenu-hiddenInput" type="checkbox" id="dropdownLinkOne" />
+
+            <div className="HeaderMenu-dropdownAdditionalLinks">
+              <a href="/futony/bavlna/">Bavlna</a>
+              <a href="/futony/kokos/">Kokos</a>
+              <a href="/futony/latex/">Latex</a>
+              <a href="/futony/detske/">Detské</a>
+              <a href="/futony/masazove/">K masážam</a>
             </div>
           </div>
-          <div className="HeaderMenu-cartLogin">
+          <div className="HeaderMenu-dropdownLink">
+            <a href="#">
+              Pohovky
+              <label title="Show additional links" htmlFor="dropdownLinkTwo">
+                {arrowDownIcon}
+              </label>
+            </a>
+            <input className="HeaderMenu-hiddenInput" type="checkbox" id="dropdownLinkTwo" />
+
+            <div className="HeaderMenu-dropdownAdditionalLinks">
+              <a href="/pohovky/80-90x200/">80-90x200 cm</a>
+              <a href="/pohovky/140x200/">140x200 cm</a>
+              <a href="/pohovky/160x200/">160x200 cm</a>
+            </div>
+          </div>
+          <div className="HeaderMenu-dropdownLink">
+            <a href="#">
+              Postele
+              <label title="Show additional links" htmlFor="dropdownLinkThree">
+                {arrowDownIcon}
+              </label>
+            </a>
+            <input className="HeaderMenu-hiddenInput" type="checkbox" id="dropdownLinkThree" />
+
+            <div className="HeaderMenu-dropdownAdditionalLinks">
+              <a href="/postele/90x200/">90x200 cm</a>
+              <a href="/postele/140x200/">140x200 cm</a>
+              <a href="/postele/160x200/">160x200 cm</a>
+              <a href="/postele/180x200/">180x200 cm</a>
+            </div>
+          </div>
+          <div className="HeaderMenu-dropdownLink">
+            <a href="#">
+              Doplnky
+              <label title="Show additional links" htmlFor="dropdownLinkFour">
+                {arrowDownIcon}
+              </label>
+            </a>
+            <input className="HeaderMenu-hiddenInput" type="checkbox" id="dropdownLinkFour" />
+            <div className="HeaderMenu-dropdownAdditionalLinks">
+              <a href="/doplnky/kresla/">Kreslá</a>
+              <a href="/doplnky/nabytok/">Nábytok</a>
+              <a href="/doplnky/vankuse/">Vankúše</a>
+            </div>
+          </div>
+        </div>
+        <div className="HeaderMenu-rightContainer">
+          <div className="HeaderMenu-rightContainerDropdownLink">
             <a href="/abc">Prihlásenie</a>
+          </div>
+          <div className="HeaderMenu-rightContainerDropdownLink">
             <a href="/bcd">{emptyCartIcon} Košík</a>
           </div>
         </div>
-        <div className="HeaderMenu-mainCategoriesFullscreen">
-          <a href="/futony/" className="HeaderMenu-dropdown">
-            <p>Futony</p>
-            <div className="HeaderMenu-dropdownHidden">
-              <a href="/futony/bavlna/">Bavlna</a>
-              <a href="/futony/kokos/">Kokos</a>
-              <a href="/futony/latex/">Latex</a>
-              <a href="/futony/detske/">Detské</a>
-              <a href="/futony/masazove/">K masážam</a>
-            </div>
-          </a>
-          <a href="/futony/" className="HeaderMenu-dropdown">
-            <p>Pohovky</p>
-            <div className="HeaderMenu-dropdownHidden">
-              <a href="/pohovky/80-90x200/">80-90x200 cm</a>
-              <a href="/pohovky/140x200/">140x200 cm</a>
-              <a href="/pohovky/160x200/">160x200 cm</a>
-            </div>
-          </a>
-          <a href="/postele/" className="HeaderMenu-dropdown">
-            <p>Postele</p>
-            <div className="HeaderMenu-dropdownHidden">
-              <a href="/postele/90x200/">90x200 cm</a>
-              <a href="/postele/140x200/">140x200 cm</a>
-              <a href="/postele/160x200/">160x200 cm</a>
-              <a href="/postele/180x200/">180x200 cm</a>
-            </div>
-          </a>
-          <a href="/doplnky/" className="HeaderMenu-dropdown">
-            <p>Doplnky</p>
-            <div className="HeaderMenu-dropdownHidden">
-              <a href="/doplnky/kresla/">Kreslá</a>
-              <a href="/doplnky/nabytok/">Nábytok</a>
-              <a href="/doplnky/vankuse/">Vankúše</a>
-            </div>
-          </a>
-        </div>
-        <div className="HeaderMenu-cartLoginFullscreen">
-          <a href="/abc">Prihlásenie</a>
-          <a href="/bcd">{emptyCartIcon} Košík</a>
-        </div>
       </div>
-    );
-    /*return (
-      <div className="HeaderMenu">
-        <div className="HeaderMenu-logoAndIcon">
-          <a href="/home">
-            <img src={logo} alt="Nejfuton logo" />
-          </a>
-          <HamburgerMenu isOpen={isOpen} onHamburgerClick={this.handleHamburgerClick} />
-        </div>
-        {isOpen && (
-          <div className="HeaderMenu-mainContentResponsive">
-            <div className="HeaderMenu-mainCategories">
-              <div className="HeaderMenu-dropdown">
-                <a href="#">
-                  Futony
-                  <span class="drop-icon">▾</span>
-                  <label title="Toggle Drop-down" class="drop-icon" for="sm1">
-                    ▾
-                  </label>
-                </a>
-                <input type="checkbox" id="sm1" />
-                <div className="sub-menu">
-                  <a href="/futony/bavlna/">Bavlna</a>
-                  <a href="/futony/kokos/">Kokos</a>
-                  <a href="/futony/latex/">Latex</a>
-                  <a href="/futony/detske/">Detské</a>
-                  <a href="/futony/masazove/">K masážam</a>
-                </div>
-              </div>
-              <div className="HeaderMenu-dropdown">
-                <a href="#">
-                  Pohovky
-                  <span class="drop-icon">▾</span>
-                  <label title="Toggle Drop-down" class="drop-icon" for="sm2">
-                    ▾
-                  </label>
-                </a>
-                <input type="checkbox" id="sm2" />
-                <div class="sub-menu">
-                  <a href="/pohovky/80-90x200/">80-90x200 cm</a>
-                  <a href="/pohovky/140x200/">140x200 cm</a>
-                  <a href="/pohovky/160x200/">160x200 cm</a>
-                </div>
-              </div>
-              <div className="HeaderMenu-dropdown">
-                <a href="#">
-                  Postele
-                  <span class="drop-icon">▾</span>
-                  <label title="Toggle Drop-down" class="drop-icon" for="sm3">
-                    ▾
-                  </label>
-                </a>
-                <input type="checkbox" id="sm3" />
-                <div className="sub-menu">
-                  <a href="/postele/90x200/">90x200 cm</a>
-                  <a href="/postele/140x200/">140x200 cm</a>
-                  <a href="/postele/160x200/">160x200 cm</a>
-                  <a href="/postele/180x200/">180x200 cm</a>
-                </div>
-              </div>
-              <div className="HeaderMenu-dropdown">
-                <a href="#">
-                  Doplnky
-                  <span class="drop-icon">▾</span>
-                  <label title="Toggle Drop-down" class="drop-icon" for="sm4">
-                    ▾
-                  </label>
-                </a>
-                <input type="checkbox" id="sm4" />
-                <div class="sub-menu">
-                  <a href="/doplnky/kresla/">Kreslá</a>
-                  <a href="/doplnky/nabytok/">Nábytok</a>
-                  <a href="/doplnky/vankuse/">Vankúše</a>
-                </div>
-              </div>
-            </div>
-            <div className="HeaderMenu-cartLogin">
-              <a href="/abc">Prihlásenie</a>
-              <a href="/bcd">{emptyCartIcon} Košík</a>
-            </div>
-          </div>
-        )}
-        <div className="HeaderMenu-mainCategoriesFullscreen">
-          <a href="/futony/" className="HeaderMenu-dropdown">
-            <p>Futony</p>
-            <div className="HeaderMenu-dropdownHidden">
-              <a href="/futony/bavlna/">Bavlna</a>
-              <a href="/futony/kokos/">Kokos</a>
-              <a href="/futony/latex/">Latex</a>
-              <a href="/futony/detske/">Detské</a>
-              <a href="/futony/masazove/">K masážam</a>
-            </div>
-          </a>
-          <a href="/futony/" className="HeaderMenu-dropdown">
-            <p>Pohovky</p>
-            <div className="HeaderMenu-dropdownHidden">
-              <a href="/pohovky/80-90x200/">80-90x200 cm</a>
-              <a href="/pohovky/140x200/">140x200 cm</a>
-              <a href="/pohovky/160x200/">160x200 cm</a>
-            </div>
-          </a>
-          <a href="/postele/" className="HeaderMenu-dropdown">
-            <p>Postele</p>
-            <div className="HeaderMenu-dropdownHidden">
-              <a href="/postele/90x200/">90x200 cm</a>
-              <a href="/postele/140x200/">140x200 cm</a>
-              <a href="/postele/160x200/">160x200 cm</a>
-              <a href="/postele/180x200/">180x200 cm</a>
-            </div>
-          </a>
-          <a href="/doplnky/" className="HeaderMenu-dropdown">
-            <p>Doplnky</p>
-            <div className="HeaderMenu-dropdownHidden">
-              <a href="/doplnky/kresla/">Kreslá</a>
-              <a href="/doplnky/nabytok/">Nábytok</a>
-              <a href="/doplnky/vankuse/">Vankúše</a>
-            </div>
-          </a>
-        </div>
-        <div className="HeaderMenu-cartLoginFullscreen">
-          <a href="/abc">Prihlásenie</a>
-          <a href="/bcd">{emptyCartIcon} Košík</a>
-        </div>
-      </div>
-    );*/
-  }
-}
+    </div>
+  );
+};
 
 export default HeaderMenu;
