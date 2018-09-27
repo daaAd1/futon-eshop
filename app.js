92cb168c9c0eeacd61886a83927fe60d1d3bf991
vangyyy <rasto.vangor@gmail.com>
2018-09-27 13:16:57 +0200

    CORS Error fix

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ app.use((req, res, next) => {
     res.header('Access-Control-Allow-Headers',
         'Origin, X-Request-With, Content-Type, Accept, Authorization');
     if (req.method === 'OPTIONS') {
-        res.header('Access-Control-Allow-Headers', 'PUT, POST, PATCH, DELETE, GET');
+        res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
         return res.status(200).json({});
     }
     return next();
